fa9c788a52dba33be64d8c3fc83d8e1955976b74
gem-cp <christian.plagens@gematik.de>
2022-10-27 11:31:40 +0200

    update

--- src/scripts/fhirsearch.rb
+++ src/scripts/fhirsearch.rb
@@ -2,13 +2,17 @@
 # Description: This script implements the part of a TI-Messenger-Client
 #              to search for FHIR ressources in the TI FHIR-Directory.
 # Parameters:  <homeserver> <mxid> <password>
-# Author:  Christian Plagens, gematik
+# Author:'https%3A%2F%2Ffhir-directory-test.vzd.ti-dienste.de%2Fsearch%2FHealthcareService%3Forganization.active%3Dtrue%20%5C%0A%26endpoint.status%3Dactive%26_include%3DHealthcareService%3Aendpoint%26_include%3DHealthcareService%3Alocation%26%20%5C%0A_include%3DHealthcareService%3Aorganization%26_pretty%3Dtrue'
+# Christian Plagens, gematik
 # Version: 0.0.1
 
 require 'faraday'
 # require 'yaml'
 require 'json'
 require 'jwt'
+require 'addressable/uri'
+# require 'erb'
+# include ERB::Util
 
 # check if mxid parameter is available
 if ARGV[0] and 3 == ARGV.length then
@@ -25,8 +29,6 @@ else
   pw = gets.chomp
 end
 
-req_header = ''
-
 # Messenger-Service login
 url = "https://#{hs}/_matrix/client/r0/login"
 
@@ -35,7 +37,7 @@ conn = Faraday.new(url) do |f|
   f.response :json
 end
 
-
+req_header = ''
 req_body = { type: 'm.login.password', user: mxid, password: pw }.to_json
 req_body_without_login = { type: 'm.login.password', user: 'mxid', password: 'password' }.to_json
 
@@ -45,19 +47,10 @@ response = conn.post do |req|
   req.body = req_body
 end
 
-# response = conn.post()
-
+# log
 file_content = "REQUEST\nPOST #{url}\nHEADER\n#{req_header}\nBODY\n#{req_body_without_login}\n"
 file_content += "RESPONSE\nSTATUS #{response.status}\nHEADER\n#{response.headers}\n"
 file_content += "BODY\n#{JSON.pretty_generate(response.body)}\n"
-# puts 'REQUEST'
-# puts "HEADER\n#{conn.headers}\n"
-# puts "BODY\n#{req_body}\n"
-
-# puts 'RESPONSE'
-# puts "STATUS\n#{response.status}\n"
-# puts "HEADER\n#{response.headers}\n"
-# puts "BODY\n#{JSON.pretty_generate(response.body)}\n"
 
 hs_access_token = response.body['access_token']
 
@@ -77,6 +70,7 @@ response = conn.post do |req|
   req.body = req_body
 end
 
+# log
 file_content += "REQUEST\nPOST #{url}\nHEADER\n#{req_header}\nBODY\n#{req_body}\n"
 file_content += "RESPONSE\nSTATUS #{response.status}\nHEADER\n#{response.headers}\n"
 file_content += "BODY\n#{JSON.pretty_generate(response.body)}\n"
@@ -99,9 +93,9 @@ response = conn.get do |req|
   req.headers['X-Matrix-OpenID-Token'] = hs_matrix_openid_token
   req.headers['X-Matrix-Server-Name'] = hs_matrix_server_name
   req_header = req.headers
-  # req.body = req_body
 end
 
+# log
 file_content += "REQUEST\nGET #{url}\nHEADER\n#{req_header}\n"
 file_content += "RESPONSE\nSTATUS #{response.status}\nHEADER\n#{response.headers}\n"
 file_content += "BODY\n#{JSON.pretty_generate(response.body)}\n"
@@ -109,7 +103,9 @@ file_content += "BODY\n#{JSON.pretty_generate(response.body)}\n"
 tim_authenticate_jwt = response.body['jwt']
 
 # FHIR /search
-url = "https://fhir-directory-test.vzd.ti-dienste.de/search/HealthcareService?organization.active=true&_count=2&_include=HealthcareService:endpoint&_include=HealthcareService:location&_include=HealthcareService:organization&_pretty=true"
+url = Addressable::URI.escape 'https://fhir-directory-test.vzd.ti-dienste.de/search/HealthcareService?organization.active=true&endpoint.status=active&_include=*&_count=2&_pretty=true'
+
+puts url
 
 conn = Faraday.new(url) do |f|
   f.request :json
@@ -120,16 +116,15 @@ response = conn.get do |req|
   req.headers['Content-Type'] = 'application/json'
   req.headers['Authorization'] = "Bearer #{tim_authenticate_jwt}"
   req_header = req.headers
-  # req.body = req_body
 end
 
 file_content += "REQUEST\nGET #{url}\nHEADER\n#{req_header}\n"
 file_content += "RESPONSE\nSTATUS #{response.status}\nHEADER\n#{response.headers}\n"
 file_content += "BODY\n#{JSON.pretty_generate(response.body)}\n"
 
-
 puts file_content
 
+# write log file
 f = File.open('../../samples/fhirsearch_sample.txt', 'w')
 f.write(file_content)
 f.close
